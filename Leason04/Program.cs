4be15e559aff7bde8a1f02c2980297bc70753dd0
Konstantin <kostik-kostik-12@mail.ru>
2023-04-22 11:21:01 +0300

    add Leason04

--- Leason04/Program.cs
+++ Leason04/Program.cs
@@ -3,7 +3,7 @@
 //8 -> 2, 4, 6, 8
 
 Console.WriteLine("Введите число");
-int number = int.Parse(Console.WriteLine());
+int number = int.Parse(Console.ReadLine());
 int numberA = 2;
 while(numberA <= number)
 {
