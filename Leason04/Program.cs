fba95fed39d1d1a5493ea024edeb37f15f931144
Konstantin <kostik-kostik-12@mail.ru>
2023-04-22 11:18:50 +0300

    add Leason04

---
+++ Leason04/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.WriteLine());
+int numberA = 2;
+while(numberA <= number)
+{
+    Console.WriteLine(numberA + " ");
+    numberA = numberA + 2;
+}
